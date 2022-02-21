d001d912e8cd30f9d7963d2f62ae632e21e0eef6
Sean Perkins <sean@ionic.io>
2022-02-21 16:14:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gh-24819-accordion-textarea',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
